2ddb3d49f1122ea0451160063a770f9c0d646e2c
guim0 <guilherme.moraes2407@gmail.com>
2020-10-01 17:11:52 -0300

    another page created

---
+++ src/sobre.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/sobre.css">
+    
+    <title>Empathea | Sobre</title>
+</head>
+<body>
+        <div class="logo">
+        <a href="sobre.php">Sobre</a>
+        </div>
+        <div class="card">
+            <div class="main-content">
+                <p>O projeto “Empathea” é uma iniciativa que visa o bem estar coletivo, 
+                    a pandemia é o momento de praticar a ajuda ao próximo, como? </p>
+
+                    <div class="items"> 
+                        <div class="services">
+                            <img src="css/assets/donation.png" alt="" >
+                            <span>Doações</span>
+                        </div>
+                        <div class="services">
+                            <img src="css/assets/shop.png" alt="" >
+                            <span>Mercadinho</span>
+                            </div>
+
+                        <div class="services">
+                            <img src="css/assets/pet.png" alt="" >
+                            <span>Pets</span>
+                            </div>
+                        
+                        <div class="services">
+                            <img src="css/assets/holding-wrench.png" alt="" >
+                            <span>Manutenção Básica</span>
+                        </div>
+                    
+               
+                </div>
+            </div>
+            
+        </div>
+
+
+    <footer> <a href="index.php">Empathea</a> </footer>
+</body>
+</html>
